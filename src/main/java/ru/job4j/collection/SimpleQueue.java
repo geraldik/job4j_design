508442ddb286b9ee535392db6cab0b420c685627
geraldik <geraldik1986@gmail.com>
2022-01-30 20:25:14 +0300

    Добавлена валидация на наличие элементов в обоих стеках.

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -8,6 +10,9 @@ public class SimpleQueue<T> {
 
 
     public T poll() {
+        if (inSize == 0 && outSize == 0) {
+            throw new NoSuchElementException();
+        }
         int count = inSize;
         if (outSize == 0) {
             outSize = inSize - 1;
