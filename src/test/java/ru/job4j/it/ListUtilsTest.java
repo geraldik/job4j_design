7015046e380c58f13be5723d16eb8724ad4454fc
geraldik <geraldik1986@gmail.com>
2022-01-31 16:58:18 +0300

    7. ListIterator [#350217]

---
+++ src/test/java/ru/job4j/it/ListUtilsTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.it;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ListUtilsTest {
+
+    @Test
+    public void whenAddBefore() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input, is(Arrays.asList(1, 2, 3)));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddBeforeWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 3, 2);
+    }
+
+    @Test
+    public void whenAddAfterLast() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
+        ListUtils.addAfter(input, 2, 3);
+        assertThat(input, is(Arrays.asList(0, 1, 2, 3)));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddAfterWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
+        ListUtils.addAfter(input, 3, 3);
+    }
+
+    @Test
+    public void whenRemoveIfEven() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+        ListUtils.removeIf(input, integer -> integer %2 == 0);
+        assertThat(input, is(Arrays.asList(1, 3)));
+    }
+
+    @Test
+    public void whenReplaceIfNotEven() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+        ListUtils.replaceIf(input, integer -> integer %2 != 0, 8);
+        assertThat(input, is(Arrays.asList(0, 8, 2, 8, 4)));
+    }
+
+    @Test
+    public void whenRemoveAll() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 2));
+        ListUtils.removeAll(input,elements);
+        assertThat(input, is(Arrays.asList(0, 3)));
+    }
+
+}
