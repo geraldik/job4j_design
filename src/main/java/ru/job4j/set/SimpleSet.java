409985b1bc98a642783f0832399a04231fd7f47a
geraldik <geraldik1986@gmail.com>
2022-02-01 14:23:14 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,44 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(10);
+
+    private int size;
+
+
+    @Override
+    public boolean add(T value) {
+        boolean rsl = false;
+        if (size == 0 || !this.contains(value)) {
+            set.add(value);
+            rsl = true;
+            size++;
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        boolean rsl = false;
+        for (T t : set) {
+            if (Objects.equals(t, value)) {
+                rsl = true;
+                break;
+            }
+
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
