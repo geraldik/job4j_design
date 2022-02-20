6c95994da08ba6c349d4bf9f58a5c3c048ce6eed
geraldik <geraldik1986@gmail.com>
2022-02-20 18:50:52 +0300

    Изменен метод unavailable

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -3,20 +3,20 @@ package ru.job4j.io;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> stringList;
-        List<String> tempList = new ArrayList<>();
+
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            stringList = read.lines().collect(Collectors.toList());
-            for (int i = 0; i < stringList.size(); i++) {
-                boolean currentState = isDown(stringList.get(i));
-                boolean beforeState = i != 0 && isDown(stringList.get(i - 1));
+            List<String> tempList = new ArrayList<>();
+            String line;
+            boolean beforeState = false;
+            while ((line = read.readLine()) != null) {
+                boolean currentState = isDown(line);
                 if (currentState != beforeState) {
-                    tempList.add(stringList.get(i).split(" ")[1]);
+                    beforeState = currentState;
+                    tempList.add(line.split(" ")[1]);
                 }
             }
             for (int i = 0, j = 1; j < tempList.size(); i = i + 2, j = j + 2) {
