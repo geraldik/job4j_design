95d5e28653f87e1d206840ff9d44fdc2ec96a3ac
geraldik <geraldik1986@gmail.com>
2022-02-20 14:28:25 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        List<String> stringList = new ArrayList<>();
+        List<String> tempList = new ArrayList<>();
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            stringList = read.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            for (int i = 0; i < stringList.size(); i++) {
+                boolean currentState = isDown(stringList.get(i));
+                boolean beforeState = i != 0 && isDown(stringList.get(i - 1));
+                if (currentState != beforeState) {
+                    tempList.add(stringList.get(i).split(" ")[1]);
+                }
+            }
+            for (int i = 0, j = 1; j < tempList.size(); i = i + 2, j = j + 2) {
+                out.printf(String.format("%s%s%s%n", tempList.get(i), ";", tempList.get(j)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isDown(String line) {
+        return line.startsWith("400")
+                || line.startsWith("500");
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("server.log", "unavailable.log");
+    }
+}
