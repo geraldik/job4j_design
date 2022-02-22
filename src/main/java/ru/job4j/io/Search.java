d4274241c580ab3196ec01f05c1ee552839b96aa
geraldik <geraldik1986@gmail.com>
2022-02-22 10:56:41 +0300

    Перенесены классы в папку main. Выделен в отдельный класс внутренний класс SearchFiles

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -20,25 +20,4 @@ public class Search {
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
-
-    private static class SearchFiles extends SimpleFileVisitor<Path> {
-        private final Predicate<Path> condition;
-        private final List<Path> paths = new ArrayList<>();
-
-        public SearchFiles(Predicate<Path> condition) {
-            this.condition = condition;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            if (condition.test(file)) {
-                paths.add(file);
-            }
-            return CONTINUE;
-        }
-
-        public List<Path> getPaths() {
-            return paths;
-        }
-    }
 }
