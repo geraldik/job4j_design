5c6ad86530e73bfd467775658a17bc72f404444d
geraldik <geraldik1986@gmail.com>
2022-02-23 11:34:56 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,17 +2,23 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Search folder is null. Usage java -jar Search.jar"
+                   + " SEARCH_FOLDER SEARCH_FILE_EXTENSION.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension is not set. Usage java -jar Search.jar"
+                    + " SEARCH_FOLDER SEARCH_FILE_EXTENSION.");
+        }
+
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
