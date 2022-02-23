5d8494bef51bfaf0ffa0a5edd3db34da101b8aa7
geraldik <geraldik1986@gmail.com>
2022-02-23 22:22:52 +0300

    Убран не используемый метод. Добавлен метод валидации входных параметров.

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -8,6 +8,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
+    private ArgsName jvm;
 
     public void packFiles(List<Path> sources, Path target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
@@ -22,30 +23,23 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(Path source, Path target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
-            zip.putNextEntry(new ZipEntry(String.valueOf(source)));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(source)))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void inputValidating(String[] args) {
+        jvm = ArgsName.of(args);
+        if (jvm.get("d") == null
+                || jvm.get("e") == null
+                || jvm.get("o") == null) {
+            throw new IllegalArgumentException("Required argument is missing.");
+        }
+        if (!Files.exists(Path.of(jvm.get("d")))) {
+            throw new IllegalArgumentException("The zipping directory is not exist.");
         }
     }
 
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        ArgsName jvm = ArgsName.of(args);
-        if (jvm.get("d") == null
-                || jvm.get("e") == null
-                || jvm.get("o") == null) {
-            throw new IllegalArgumentException(" Required argument is missing.");
-        }
-        if (!Files.exists(Path.of(jvm.get("d")))) {
-            throw new IllegalArgumentException("The zipping directory is not exist.");
-        }
-        List<Path> sources = Search.search(Path.of(jvm.get("d")),
-                p -> !p.toFile().getName().endsWith(jvm.get("e")));
-        zip.packFiles(sources, Path.of(jvm.get("o")));
+        zip.inputValidating(args);
+        List<Path> sources = Search.search(Path.of(zip.jvm.get("d")),
+                p -> !p.toFile().getName().endsWith(zip.jvm.get("e")));
+        zip.packFiles(sources, Path.of(zip.jvm.get("o")));
     }
 }
