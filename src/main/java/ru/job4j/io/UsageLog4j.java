6fd03032fd52c5cda819816c19513b2ed6a44f06
geraldik <geraldik1986@gmail.com>
2022-03-06 20:32:30 +0300

    Указано имя класса

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger("UsageLog4j");
 
     public static void main(String[] args) {
         String name = "Vlad";
