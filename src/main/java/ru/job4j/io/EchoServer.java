495d9398cdac387d1526255f2adb2fd106f5243d
geraldik <geraldik1986@gmail.com>
2022-03-06 21:44:59 +0300

    Исправлено имя класса в аргументах метода getLogger

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 import org.slf4j.Logger;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
