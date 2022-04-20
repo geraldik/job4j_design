24df9584dd9e60250c70ff4f102f68d78817d9c5
geraldik <geraldik1986@gmail.com>
2022-04-20 22:44:53 +0300

    Update

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -9,25 +9,18 @@ public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-            cache.put(key, new SoftReference<V>(value));
+            cache.put(key, new SoftReference<>(value));
     }
 
     public V get(K key) {
-        V rsl;
-        if (cache.get(key) != null) {
-            rsl = cache.get(key).get();
-            if (cache.get(key).get() == null) {
+        V rsl = cache.getOrDefault(key, new SoftReference<>(null)).get();
+            if (rsl == null) {
                 rsl = load(key);
             } else {
                 rsl = cache.get(key).get();
             }
-        } else {
-            put(key, load(key));
-            rsl = cache.get(key).get();
-        }
         return rsl;
     }
 
     protected abstract V load(K key);
-
 }
