4fb19d8b37f2dae60b1aaa16120ccd8a656401b4
geraldik <geraldik1986@gmail.com>
2022-04-20 14:27:41 +0300

    Delete comments

--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -1,7 +1,6 @@
 package ru.job4j.cache;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class Emulator {
 
@@ -10,31 +9,6 @@ public class Emulator {
     public Emulator(Output out) {
         this.out = out;
     }
-
-
-
-//    private void loadDir(String dir) {
-//        dirFileCache = new DirFileCache(dir);
-//    }
-//
-//    private void cacheFile(String name) {
-//        if (dirFileCache == null) {
-//            System.out.println("First you need to load the directory path.");
-//        } else {
-//            dirFileCache.load(name);
-//        }
-//    }
-
-//    private String readCache(String name) {
-//        String rsl = null;
-//        if (dirFileCache == null) {
-//            System.out.println("First you need to load the directory path.");
-//
-//        } else {
-//            rsl = dirFileCache.get(name);
-//        }
-//        return rsl;
-//    }
 
     public void init(Input input, List<UserAction> actions) {
         boolean run = true;
@@ -67,8 +41,5 @@ public class Emulator {
                 new ExitAction()
         );
         new Emulator(output).init(input, actions);
-//        System.out.println("Enter path for caching directory:");
-//        emulator.cacheFile("Names.txt");
-//        System.out.println(emulator.readCache("Names.txt"));
     }
 }
