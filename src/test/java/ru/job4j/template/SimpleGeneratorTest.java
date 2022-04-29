deac0da64a7dfc14f8961002bdb3ebf83963e2be
geraldik <geraldik1986@gmail.com>
2022-04-29 15:04:49 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/SimpleGeneratorTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SimpleGeneratorTest {
+
+    @Test
+    public void whenKeysAreRight() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("subject", "you");
+        String expected = "I am Petr, Who are you?";
+        String result = generator.produce(template, map);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenKeyUsingTwice() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am ${name}, Who are ${subject}? I am ${name}.";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("subject", "you");
+        String expected = "I am Petr, Who are you? I am Petr.";
+        String result = generator.produce(template, map);
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenOneKeyIsWrong() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("surname", "Ivanov");
+        String result = generator.produce(template, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenOneKeyIsExcess() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("subject", "you");
+        map.put("surname", "Ivanov");
+        String result = generator.produce(template, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenMissingOneKey() {
+        Generator generator = new SimpleGenerator();
+        String template = "${greeting}. I am ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("subject", "you");
+        String result = generator.produce(template, map);
+    }
+}
