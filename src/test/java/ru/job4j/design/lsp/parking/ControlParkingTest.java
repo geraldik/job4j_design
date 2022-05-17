c848a295d88c74ba4fddbf465ee0ce1730501e26
geraldik <geraldik1986@gmail.com>
2022-05-17 20:14:55 +0300

    CheckStyle

--- src/test/java/ru/job4j/design/lsp/parking/ControlParkingTest.java
+++ src/test/java/ru/job4j/design/lsp/parking/ControlParkingTest.java
@@ -9,7 +9,7 @@ public class ControlParkingTest {
 
     @Test @Ignore
     public void whenTwoPassengerAndOneTruckPlacesThanTwoTrucks() {
-        var parking = new ControlParking (2, 1);
+        var parking = new ControlParking(2, 1);
         var truck = new Truck(2);
         assertTrue(parking.park(truck));
         assertTrue(parking.park(truck));
@@ -18,7 +18,7 @@ public class ControlParkingTest {
 
     @Test @Ignore
     public void whenTwoPassengerAndOneTruckPlacesThanOneCarAndTwoTrucks() {
-        var parking = new ControlParking (2, 1);
+        var parking = new ControlParking(2, 1);
         var car = new PassengerCar();
         var truck = new Truck(2);
         assertTrue(parking.park(car));
@@ -30,7 +30,7 @@ public class ControlParkingTest {
 
     @Test @Ignore
     public void whenTwoPassengerAndNoTruckPlacesThanOneTruckAndNoCar() {
-        var parking = new ControlParking (2, 0);
+        var parking = new ControlParking(2, 0);
         var car = new PassengerCar();
         var truck = new Truck(2);
         assertTrue(parking.park(truck));
@@ -40,7 +40,7 @@ public class ControlParkingTest {
 
     @Test @Ignore
     public void whenNoPassengerAndOneTruckPlacesThanNoCar() {
-        var parking = new ControlParking (0, 1);
+        var parking = new ControlParking(0, 1);
         var car = new PassengerCar();
         assertFalse(parking.park(car));
     }
